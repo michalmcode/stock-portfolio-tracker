317c7accccd923638665537c3bcf4e8d052defa9
michalm138 <michalmichalak138@gmail.com>
2025-01-24 23:11:54 +0100

    project initialization

---
+++ src/main/java/com/michalmcode/stockportfoliotracker/StockportfoliotrackerApplication.java
@@ -0,0 +1,13 @@
+package com.michalmcode.stockportfoliotracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockportfoliotrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockportfoliotrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/michalmcode/stockportfoliotracker/StockportfoliotrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.michalmcode.stockportfoliotracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockportfoliotrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
