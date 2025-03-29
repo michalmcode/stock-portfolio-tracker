d94faf77410256a55bef2ed0804fd483c12d7442
michalm138 <michalmichalak138@gmail.com>
2025-03-29 16:00:31 +0100

    feat: fill other fields during patch update

--- src/main/java/com/michalmcode/stockportfoliotracker/stock/StockService.java
+++ src/main/java/com/michalmcode/stockportfoliotracker/stock/StockService.java
@@ -2,6 +2,8 @@ package com.michalmcode.stockportfoliotracker.stock;
 
 import org.springframework.stereotype.Service;
 
+import java.time.temporal.ChronoUnit;
+
 @Service
 public class StockService {
     private final StockRepository repository;
@@ -18,6 +20,13 @@ public class StockService {
 
     public Stock update(Stock stock, UpdateStockDto stockDto) {
         StockMapper.INSTANCE.updateStockFromDto(stockDto, stock);
+        stock.setOpenValue(stock.getVolume() * stock.getOpenPrice());
+        stock.setCloseValue(stock.getVolume() * stock.getClosePrice());
+        stock.setReturnValue(stock.getCloseValue() - stock.getOpenValue());
+
+        stock.setDuration((int) ChronoUnit.DAYS.between(stock.getOpenDate(), stock.getCloseDate()));
+        stock.setDurationUnit("days");
+
         repository.save(stock);
         return stock;
     }
